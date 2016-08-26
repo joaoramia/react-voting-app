0c95e2bf70fb65bee31cb9721e6c2f467fcc103c
joaoramia <joao.ramia@gmail.com>
2016-08-26 00:54:40 -0300

    created the reducer specs and finalized the voting logic

---
+++ src/reducer.js
@@ -0,0 +1,16 @@
+import {setEntries, next, vote, INITIAL_STATE} from './core';
+
+//this is for the Redux actions:
+export default function reducer(state = INITIAL_STATE, action){
+	switch (action.type){
+		case 'SET_ENTRIES':
+			return setEntries(state, action.entries);
+		case 'NEXT':
+			return next(state);
+		case 'VOTE':
+			return vote(state, action.entry);
+	}
+
+	//if the reducer doesn't recognize the action, it just returns the current state
+	return state;
+}
